a737b3e49afbf27bafbcd1355e788d5a51b3b96d
RÔMULO RODRIGUES DE OLIVEIRA <68561301+Romuloro@users.noreply.github.com>
2022-01-21 07:31:18 -0300

    commit_21_01_2022_1

---
+++ Javascript_Estrutura_de_Controle/if.js
@@ -0,0 +1,12 @@
+function soBoaNoticia(nota){
+    if (nota >= 7){
+        console.log('Aprovado com ', nota)
+    }
+    else{
+        console.log('Reprovado com ', nota)
+    }
+}
+
+soBoaNoticia(9.5)
+soBoaNoticia(5.6)
+
---
+++ Javascript_Estrutura_de_Controle/if_else_if.js
@@ -0,0 +1,26 @@
+Number.prototype.entre = function(inicio, fim) {
+    return this >= inicio && this <= fim
+    }
+
+const imprimirResultado = function(nota){
+    if (nota.entre(9,10)){
+        console.log("Quadro de Honra")
+    }
+    else if (nota.entre(7,8.99)){
+        console.log("Aprovado")
+    }
+    else if (nota.entre(6.99, 4)){
+        console.log("Recuperação")
+    }
+    else if (nota.entre(3.99, 0)){
+        console.log("Reprovado")
+    }
+    else{
+        console.log("Nota inválida")
+    }
+    
+}
+
+imprimirResultado(5)
+imprimirResultado(2.25)
+imprimirResultado(9.25)
