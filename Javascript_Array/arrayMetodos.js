bb19e7c6e06436bb078d10ab60d7cca290c2f408
RomuloRo <rrorodriguesoliveira@gmail.com>
2022-02-02 17:53:41 -0300

    commit_02_02_2022_2

---
+++ Javascript_Array/arrayMetodos.js
@@ -0,0 +1,9 @@
+const times = ['Flamengo', 'Fluminense', 'Vasco', 'Atlético']
+times.pop() //Retirar o último da array
+console.log(times)
+
+times.push('Botafogo') //inclue no final da array
+console.log(times)
+
+times.shift() //remove o primeiro
+console.log()
