d35ad7520232a8e26382467e1b5c2f008191f6db
RomuloRo <rrorodriguesoliveira@gmail.com>
2022-02-04 12:14:58 -0300

    commit_04_02_2022_2

--- node/projeto1/src/server.js
+++ node/projeto1/src/server.js
@@ -2,9 +2,28 @@ const door = 3003
 
 const express = require('express')
 const app = express()
+const bodyParse = require('body-parser')
+const bancoDeDados = require('./bancoDados')
+
+app.use(express.json())
+
+app.use(bodyParse.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 7500.78}) // converter para Json
+    res.send(bancoDeDados.getProdutos()) // converter para Json
+})
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    console.log(req.body)
+     const produto = bancoDeDados.salvarProduto({
+         nome: req.body.nome,
+         preco: req.body.preco
+     })
+    res.send(produto) //Gera um JSON
 })
 
 app.listen(door, () => {
