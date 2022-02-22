48b1c51ea59f8e043fb7d4cd6678c63aa71f7352
RomuloRo <rrorodriguesoliveira@gmail.com>
2022-02-22 19:35:33 -0300

    commit_22_02_2022

---
+++ ajax/server.js
@@ -0,0 +1,32 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const multer = require('multer')
+
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(req, file,callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) =>{
+    upload(req, res, err =>{
+        if(err){
+            return res.end("Deu ruim")
+        }
+        res.end('Deu bom')
+    })
+})
+
+const door = 3003
+app.listen(door, () => console.log(`Executando na porta ${door}`))
